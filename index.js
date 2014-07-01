b8079926e692ed4212bcbdc16a63fc3a7c60dc72
droghio <business@jldapps.com>
2014-07-01 10:52:59 -0400

    Fixed offset, shows interesting shapes.

--- index.js
+++ index.js
@@ -9,10 +9,16 @@ function updateSections(){
     
     $("#circle").empty()
     
-    theta = 360/n
+    theta = 2*Math.PI/n
     
     for (i=0; i < n; i++){
-        $("#circle").append('<div class="line" style="-webkit-transform: rotate(' + theta*i + 'deg);"><div class="marker" style="-webkit-transform: rotate(' + -theta*i + 'deg);"></div></div>');
+        $("#circle").append(
+            '<div class="line" style="-webkit-transform: rotate(' + theta*i + 'rad);">\
+                    <div class="marker" style="-webkit-transform: rotate(' + -theta*i + 'rad);">\
+                            <div class="markerinner"></div>\
+                    </div>\
+            </div>'
+        );
     }
     
     //Update newly created markers.
@@ -28,3 +34,10 @@ function resizeSections(){
     $(".marker").css("width", t+"px")
     $(".marker").css("margin", -t/2-1+"px")
 }
+
+
+function findCenter(n){
+    theta = 2*Math.PI/n;
+}
+
+function slope(p1, p2){return (p2[1] - p1[1]) / (p2[0] - p1[0])}
